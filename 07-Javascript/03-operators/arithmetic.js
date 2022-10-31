6afd6c1d7682b1acbf1c0146f66aac51c13bc813
mdFahimMorshed <contactahmfahim@gmail.com>
2022-10-31 10:26:41 +0600

    js arithmetic

---
+++ 07-Javascript/03-operators/arithmetic.js
@@ -0,0 +1,48 @@
+/* JavaScript Arithmetic Operators
+Arithmetic operators perform arithmetic on numbers (literals or variables).
+
+Operator	Description
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Remainder)
+++	Increment
+--	Decrement
+ */
+
+let x = 5, y = 29;
+let z = x / y + y * 2 - 10;
+console.log(z);
+
+
+// Remainder
+// The modulus operator (%) returns the division remainder.
+
+// Example
+let a = 5;
+let b = 2;
+let c = a % b;
+console.log(c);
+
+
+//  INCREMENTING
+
+let incriment = 44;
+incriment++;
+console.log(incriment);
+
+//Decrementing
+
+let Decrement = 5;
+Decrement--;
+console.log(Decrement);
+
+// Operator Precedence
+// Operator precedence describes the order in which operations are performed in an arithmetic expression.
+
+// Example
+let precedence = 100 + 50 * 3;
+
+console.log(precedence);
