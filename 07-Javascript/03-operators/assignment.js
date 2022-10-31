84906cd805f508a850a00f128a4e8a6ea8131056
mdFahimMorshed <contactahmfahim@gmail.com>
2022-10-31 10:32:59 +0600

    assignment operator

---
+++ 07-Javascript/03-operators/assignment.js
@@ -0,0 +1,30 @@
+/* 
+JavaScript Assignment Operators
+Assignment operators assign values to JavaScript variables.
+
+Operator	Example	        Same As
+=	        x = y	        x = y
++=	        x += y	        x = x + y
+-=	        x -= y	        x = x - y
+*=	        x *= y	        x = x * y
+/=	        x /= y	        x = x / y
+%=	        x %= y	        x = x % y
+**=	        x **= y	        x = x ** y
+
+*/
+
+/*
+Shift Assignment Operators
+Operator	Example	        Same As
+<<=	        x <<= y	        x = x << y
+>>=	        x >>= y	        x = x >> y
+>>>=	    x >>>= y	    x = x >>> y
+*/
+
+/*
+Logical Assignment Operators
+Operator	Example	        Same As
+&=	        x &= y	        x = x & y
+^=	        x ^= y	        x = x ^ y
+|=	        x |= y	        x = x | y
+*/
