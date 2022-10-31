9c78a2bf08db2941dd03e5f32b07a0dbf973af77
mdFahimMorshed <contactahmfahim@gmail.com>
2022-10-31 09:10:01 +0600

    js const

--- 07-Javascript/02-varLetConst/const.js
+++ 07-Javascript/02-varLetConst/const.js
@@ -1,1 +1,32 @@
- 
+// The const keyword was introduced in ES6(2015)
+//Variables defined with const cannot be Redeclared.
+
+//Variables defined with const cannot be Reassigned.
+
+//Variables defined with const have Block Scope.
+var x = 2; // Allowed
+const x = 2; // Not allowed
+
+{
+    let x = 2; // Allowed
+    const x = 2; // Not allowed
+}
+
+{
+    const x = 2; // Allowed
+    const x = 2; // Not allowed
+}
+
+//=====================
+const x = 2;     // Allowed
+x = 2;           // Not allowed
+var x = 2;       // Not allowed
+let x = 2;       // Not allowed
+const x = 2;     // Not allowed
+
+{
+  const x = 2;   // Allowed
+  x = 2;         // Not allowed
+  var x = 2;     // Not allowed
+  let x = 2;     // Not allowed
+  const x = 2;   // Not allowed
