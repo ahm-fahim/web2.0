5600da4180bd5f5928d1a650cb5a677e0c950bc9
mdFahimMorshed <contactahmfahim@gmail.com>
2022-10-31 09:03:10 +0600

    js let

---
+++ 07-Javascript/02-varLetConst/let.js
@@ -0,0 +1,7 @@
+//The "let" keyword was introduced in ES6(2015)
+//Variables defined with let cannot be redeclared.
+let x = "John Doe";
+
+// let x = 0;
+
+// SyntaxError: 'x' has already been declared
