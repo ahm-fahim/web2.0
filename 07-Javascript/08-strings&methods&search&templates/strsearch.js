bba9790089fe8e3b090c24627fcddd1eb5539166
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-02 21:37:12 +0600

    string search

---
+++ 07-Javascript/08-strings&methods&search&templates/strsearch.js
@@ -0,0 +1,25 @@
+/*/````````````````````````````\*\
+
+\*\______.______.______.______./*/
+
+/*/````````````````````````````\*\
+    JavaScript Search Methods
+    _________________________
+
+    String indexOf()
+    String lastIndexOf()
+    String search()
+    String match()
+    String matchAll()
+    String includes()
+    String startsWith()
+    String endsWith()
+\*\______.______.______.______./*/
+
+/*/````````````````````````````\*\
+            indexOf()
+\*\______.______.______.______./*/
+
+let str = "Please locate where \"Locate\" occurs!"
+
+console.log(str.indexOf('locate'));
