f4c1056cee9cd893ecc915f5198b17dbfb3f7b9e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-03 12:59:21 +0600

    js numbers

---
+++ 07-Javascript/09-numbers/numbers.js
@@ -0,0 +1,16 @@
+
+  /*~~~~~~~~~~~~~~~~~*\
+ /*                   *\
+/*      JavaScript     *\
+\*        Number        /
+ \*                    /
+  \*~~~~~~~~~~~~~~~~~*/
+
+
+let x = 3.14; //a number with decimals
+let y = 3; //a number without decimals
+
+let a = 123e5; //12300000
+let b = 123e-5; //0.00123
+
+console.log(x, y, a, b);
