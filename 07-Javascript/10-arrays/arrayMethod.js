cbb488a9e32b87f2b3d835c2f69d51146a2b224b
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-13 12:36:43 +0600

    js array method

--- 07-Javascript/10-arrays/arrayMethod.js
+++ 07-Javascript/10-arrays/arrayMethod.js
@@ -17,3 +17,28 @@ document.getElementById("pushMethod").innerHTML = fruits;
 //shift() method 
 fruits.shift();
 document.getElementById("shiftMethod").innerHTML = fruits;
+
+//unshift() method
+
+fruits.unshift("Lemon");
+document.getElementById("unshiftMethod").innerHTML = fruits;
+
+//changing array elements
+fruits[0] = "Passion";
+document.getElementById("changingElements").innerHTML = fruits;
+
+// length methods
+fruits[fruits.length] = "Kiwi";
+document.getElementById("lengthMethod").innerHTML = fruits;
+
+//delete method
+delete fruits[4];
+document.getElementById("deleteMethod").innerHTML = fruits;
+
+// merging  cocat
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+const myChildren = myGirls.concat(myBoys);
+
+document.getElementById("mergingMethod").innerHTML = myChildren;
