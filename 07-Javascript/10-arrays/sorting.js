3f68d466d8b5eb14817d568fd48ceb735c4819d9
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-13 21:50:34 +0600

    js array method

--- 07-Javascript/10-arrays/sorting.js
+++ 07-Javascript/10-arrays/sorting.js
@@ -1,3 +1,22 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
 document.getElementById("sort").innerHTML = fruits.sort();
+
+// reverse Method;
+
+document.getElementById("reverseMethod").innerHTML = fruits.reverse();
+
+
+//numeric sorts
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function (a, b) {
+    return a - b;
+});
+
+document.getElementById("numericSort").innerHTML = points;
+
+points.sort(function (a, b) {
+    return b - a;
+})
+document.getElementById("numericSort2").innerHTML = points;
