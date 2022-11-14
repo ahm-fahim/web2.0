a96053f2f1751534d58e49585fc68d03e771e64e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-14 23:03:03 +0600

    array sort

--- 07-Javascript/10-arrays/sorting.js
+++ 07-Javascript/10-arrays/sorting.js
@@ -8,7 +8,6 @@ document.getElementById("reverseMethod").innerHTML = fruits.reverse();
 
 
 //numeric sorts
-
 const points = [40, 100, 1, 5, 25, 10];
 points.sort(function (a, b) {
     return a - b;
