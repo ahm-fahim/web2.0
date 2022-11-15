d34bffcac7b9c5f099d0ad130d4654f89f7df896
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-15 12:08:58 +0600

    array sorts

--- 07-Javascript/10-arrays/sorting.js
+++ 07-Javascript/10-arrays/sorting.js
@@ -63,11 +63,74 @@ nums.sort(function (a, b) {
 });
 document.getElementById("minValue").innerHTML = nums;
 
-
 //maxMethod
 
 function arrMax(arr) {
     return Math.max.apply(null, arr);
 }
 
-document.getElementById("maxMethod").innerHTML= arrMax(nums);
+document.getElementById("maxMethod").innerHTML = arrMax(nums);
+
+//minMethod
+
+function arrMin(arr) {
+    return Math.min.apply(null, arr);
+}
+document.getElementById("minMethod").innerHTML = arrMin(nums);
+
+// customMax/Min
+function customMax(arr) {
+    let len = arr.length;
+    let max = -Infinity;
+    while (len--) {
+        if (arr[len] > max) {
+            max = arr[len];
+        }
+    }
+    return max;
+}
+document.getElementById("customMax").innerHTML = customMax(nums);
+
+// customMin
+
+function customMin(arr) {
+    let len = arr.length;
+    let min = Infinity;
+    while (len--) {
+        if (arr[len] < min) {
+            min = arr[len];
+        }
+    }
+    return min;
+}
+document.getElementById("customMin").innerHTML = customMin(nums);
+
+//objectSort
+
+const cars = [
+    { type: "Volvo", year: 2016 },
+    { type: "Saab", year: 2001 },
+    { type: "BMW", year: 2010 },
+];
+
+displayCars();
+
+cars.sort(function (a, b) {
+    return a.year - b.year;
+});
+displayCars();
+
+function displayCars() {
+    document.getElementById("objectSort").innerHTML =
+        cars[0].type +
+        " " +
+        cars[0].year +
+        "<br>" +
+        cars[1].type +
+        " " +
+        cars[1].year +
+        "<br>" +
+        cars[2].type +
+        " " +
+        cars[2].year;
+}
