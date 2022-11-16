0d1f15043d86daa859a4c705b7a9db8d5d6071dd
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-16 19:10:46 +0600

    array iterator

--- 07-Javascript/10-arrays/arrayIteration.js
+++ 07-Javascript/10-arrays/arrayIteration.js
@@ -1,9 +1,62 @@
-const numbers = [45, 4, 9, 16, 25];
-let txt = "";
-numbers.forEach(myFunction);
+const nums = [33, 44, 53, 65, 23, 64, 12];
 
-function myFunction(value, index, array) {
-    txt += value + "<br>";
+let emty = "";
+
+// JS forEach()
+nums.forEach((element) => {
+    console.log(element);
+    emty += element + "<br>";
+    document.getElementById("forEach").innerHTML = emty;
+});
+
+// JS map()
+const numsMap = nums.map(mapFunc);
+document.getElementById("map").innerHTML = numsMap;
+
+function mapFunc(value, index, array) {
+    return value * 2;
 }
 
-console.log(numbers);
+
+//filter()
+
+const under18 = nums.filter(filterFunc);
+document.getElementById("filter").innerHTML = under18;
+function filterFunc(value, index, array) {
+    return value < 18;
+}
+
+//reduce()
+
+let numsReduce = nums.reduce(reduceFunc);
+document.getElementById("reduce").innerHTML = numsReduce;
+function reduceFunc(total, value, index, array) {
+    return total + value;
+}
+
+// initalReduce
+
+let initialReduce = nums.reduce(initialReduceFunc, 100);
+
+document.getElementById("initialReduce").innerHTML = initialReduce;
+function initialReduceFunc(total, value) {
+    return total + value;
+}
+
+//reduceRight
+
+let reduceRight = nums.reduceRight(reduceRightFunc);
+document.getElementById("reduceRight").innerHTML = reduceRight;
+
+function reduceRightFunc(total, value){
+    return total + value;
+}
+
+//every()
+let allOver18 = nums.every(everyFunc);
+
+document.getElementById("every").innerHTML = allOver18;
+
+function everyFunc(value, index, array) {
+    return value > 18;
+}
