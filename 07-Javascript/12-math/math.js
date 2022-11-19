938709b9d4afe439da434c12f03d63fbc14b4e9e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-19 23:00:14 +0600

    ceil round floor method

--- 07-Javascript/12-math/math.js
+++ 07-Javascript/12-math/math.js
@@ -25,3 +25,36 @@ Math.LOG2E    // returns base 2 logarithm of E
 Math.LOG10E   // returns base 10 logarithm of E
 */
 
+// Math Methods
+
+//The syntax for Math any methods is : Math.method(number)
+
+//Number to Integer
+
+/*
+There are 4 common methods to round a number to an integer:
+
+Math.round(x)	Returns x rounded to its nearest integer
+Math.ceil(x)	Returns x rounded up to its nearest integer
+Math.floor(x)	Returns x rounded down to its nearest integer
+Math.trunc(x)	Returns the integer part of x (new in ES6)
+*/
+
+//Math.round()
+//Math.round(x) returns the nearest integer:
+console.log("Math.round()", Math.round(4.5));
+console.log(Math.round(4.2));
+
+// Math.ceil()
+
+// Math.ceil(x) returns the value of x rounded up to its nearest integer:
+console.log(
+    "Math.ceil ",
+    Math.ceil(4.9),
+    Math.ceil(4.7),
+    Math.ceil(4.4),
+    Math.ceil(4.2),
+    Math.ceil(-4.2)
+);
+
+//Math.floor()
