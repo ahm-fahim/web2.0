7fe049e1b6147806c015da8f94b713e3d4d94451
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-19 21:32:34 +0600

    js math

--- 07-Javascript/12-math/math.js
+++ 07-Javascript/12-math/math.js
@@ -1,1 +1,27 @@
 console.log(Math.PI);
+
+/*
+
+Unlike other objects, the Math object has no constructor.
+
+The Math object is static.
+
+All methods and properties can be used without creating a Math object first.*/
+
+/*
+Math Properties (Constants)
+The syntax for any Math property is : Math.property.
+
+JavaScript provides 8 mathematical constants that can be accessed as Math properties:
+
+Example
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
+*/
+
