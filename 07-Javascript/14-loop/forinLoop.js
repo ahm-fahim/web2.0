e5f36848cc1db60dfee705ef2e3f719335bb86ca
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-20 18:49:44 +0600

    for in loop

--- 07-Javascript/14-loop/forinLoop.js
+++ 07-Javascript/14-loop/forinLoop.js
@@ -16,3 +16,18 @@ const person = { name: "Fahim", age: 23, height: 5.7 };
 for (let i in person) {
     console.log(i, ":", person[i]);
 }
+
+// array
+
+const nums = [12, 32, 43, 54, 65];
+for (let i in nums) {
+    console.log(nums[i]);
+}
+
+//forEach()
+let emtyNums = "";
+nums.forEach(numsFunc);
+console.log(emtyNums);
+function numsFunc(value) {
+    emtyNums += value + ", ";
+}
