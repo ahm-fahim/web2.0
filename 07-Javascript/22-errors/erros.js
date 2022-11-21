a5cf2e6b5c68e632cb7b86bdb9df488dd5a3a71f
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-21 22:59:09 +0600

    errors

---
+++ 07-Javascript/22-errors/erros.js
@@ -0,0 +1,18 @@
+/* 
+Throw, and Try...Catch...Finally
+The try statement defines a code block to run (to try).
+
+The catch statement defines a code block to handle any error.
+
+The finally statement defines a code block to run regardless of the result.
+
+The throw statement defines a custom error.
+
+
+*/
+
+try {
+    adddlert("welcome!");
+} catch (err) {
+    console.log(err.message);
+}
