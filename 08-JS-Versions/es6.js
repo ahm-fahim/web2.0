5b4c536936f5838606177e9db3796fcc12c169db
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-26 23:58:58 +0600

    es6 done upto js promise

--- 08-JS-Versions/es6.js
+++ 08-JS-Versions/es6.js
@@ -72,3 +72,61 @@ console.log(combined);
 const nums = [2, 1, 43, 55, 64, 76, 32];
 let mxVal = Math.max(...nums);
 console.log(mxVal);
+
+//for of loop
+
+for (let x of cars2) {
+    console.log(x);
+}
+
+//maps
+const fruits = new Map([
+    ["apple", 500],
+    ["mango", 200],
+    ["orange", 300],
+]);
+console.log(fruits.get("apple"));
+
+//sets
+
+const letters = new Set();
+
+letters.add("a");
+console.log(letters);
+
+//class
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+}
+
+const car1 = new Car("Ford", 2012);
+console.log(car1);
+
+//JavaScript Promises
+
+// Promise Syntax
+// const myPromise = new Promise(function(myResolve, myReject) {
+// // "Producing Code" (May take some time)
+
+//   myResolve(); // when successful
+//   myReject();  // when error
+// });
+
+// // "Consuming Code" (Must wait for a fulfilled Promise).
+// myPromise.then(
+//   function(value) { /* code if successful */ },
+//   function(error) { /* code if some error */ }
+// );
+
+const myPromise = new Promise(function (myResolve, myReject) {
+    setTimeout(function () {
+        myResolve("I love you!!");
+    }, 3000);
+});
+myPromise.then(function (val) {
+    console.log(val);
+});
+
