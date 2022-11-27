ad34d8d295c78ae49f17680cbac45859bb65993e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-27 11:45:27 +0600

    es6 complete upto array findIndex

--- 08-JS-Versions/es6.js
+++ 08-JS-Versions/es6.js
@@ -130,3 +130,77 @@ myPromise.then(function (val) {
     console.log(val);
 });
 
+/*
+The Symbol Type
+A JavaScript Symbol is a primitive datatype just like Number, String, or Boolean.
+
+It represents a unique "hidden" identifier that no other code can accidentally access.
+
+For instance, if different coders want to add a person.id property to a person object belonging to a third-party code, they could mix each others values.
+
+Using Symbol() to create a unique identifiers, solves this problem:
+*/
+const person = {
+    firstName: "Fahim",
+    lastName: "Morshed",
+    age: 23,
+    eyeColor: "black",
+};
+let id = Symbol("id");
+person[id] = 123456;
+console.log("Fahim Id = ", person[id] + " " + person.id);
+
+//default parameter values
+
+function peraFunc(x, y = 10) {
+    return x + y;
+}
+peraFunc(10);
+
+//Function Rest Parameter
+
+// he rest parameter (...) allows a function to treat an indefinite number of arguments as an array:
+
+function sum(...args) {
+    let sum = 0;
+    for (let arg of args) sum += arg;
+    return sum;
+}
+let restSum = sum(4, 9, 16, 25, 29, 100, 66, 77);
+console.log(restSum);
+
+//includes
+let text = "Hello World";
+console.log(text);
+
+console.log(text.includes("World"));
+
+//startsWith()
+
+console.log(text.startsWith("H"));
+
+//endsWith()
+console.log(text.endsWith("d"));
+
+//array.from()
+console.log(Array.from("abcdef"));
+
+//Array keys()
+
+const keys = fruits.keys();
+
+let text2 = "";
+for (let x of keys) {
+    console.log(x);
+}
+
+//find()
+
+let find = nums.find(findFunc);
+console.log(find);
+function findFunc(a) {
+    return a > 18;
+}
+
+
+//Array findIndex()
