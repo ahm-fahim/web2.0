7d44e7f90c428d301244d1058a689adbc668b425
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-29 19:04:18 +0600

    todo list

--- 10.JS-DOM/02-domPractice/todo.js
+++ 10.JS-DOM/02-domPractice/todo.js
@@ -1,13 +1,28 @@
-const todoInput = document.getElementById('todo-input');
-const addBtn = document.getElementById('add-btn');
-const listContainer = document.getElementById("list-container");
-const item = document.getElementById('item');
+const todoInput = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-btn");
+const listContainer = document.getElementById("list-container"); 
+const items = document.getElementsByClassName('item');
 
-addBtn.addEventListener('click', () => {
-    const li = document.createElement('li');
-
-    const html = `
-        <li class="item">{todoInput.value}</li>
-    `
-    listContainer.appendChild('html');
+//---------------------------------------
+// delete items 
+// for (const item of items) {
+//     item.addEventListener('click', (e) => {
+//         e.target.parentNode.removeChild(e.target);
+//     })
+// }
+//----------------------------------------------
+// add items
+addBtn.addEventListener("click", () => {
+    const li = document.createElement("li");
+    li.classList.add('item');
+    li.innerHTML = `
+    ${todoInput.value}
+    `;
+    listContainer.appendChild(li);
+    todoInput.value = " ";
+});
+//delete items
+listContainer.addEventListener('click', (e) => {
+    e.target.parentNode.removeChild(e.target);
 })
+
