69fa3422acd52aada022574acfab5f5f341cd371
mdFahimMorshed <contactahmfahim@gmail.com>
2022-11-30 12:44:25 +0600

    secoure deposit

--- 10.JS-DOM/03-dom-project/01-deposit/deposit.js
+++ 10.JS-DOM/03-dom-project/01-deposit/deposit.js
@@ -1,3 +1,23 @@
-document.getElementById("deposit-btn").addEventListener('click', () => {
-    console.log("click");
-})
+document.getElementById("deposit-btn").addEventListener("click", () => {
+
+    // get deposit amount
+    const getdepoInputId = document.getElementById("deposit-input");
+    const depoInputText = getdepoInputId.value;
+    // convert str to nums
+    const depoInput = parseFloat(depoInputText);
+
+    // get deposit display id
+    const depoAmountId = document.getElementById("deposit-amount");
+    const depoAmountText = depoAmountId.innerText;
+    // convert str to nums
+    const prevDepoAmount = parseFloat(depoAmountText);
+
+    // add(+) new deposit amount
+    const newDepoAmount = prevDepoAmount + depoInput;
+
+    //set deposit amount on display id
+    depoAmountId.innerText = newDepoAmount;
+
+    //clear deposit input value
+    getdepoInputId.value = " ";
+});
