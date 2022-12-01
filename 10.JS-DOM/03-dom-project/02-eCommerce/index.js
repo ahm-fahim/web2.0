af7c20f85c6add70dcd98165ea0866f46d6fbd82
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-01 16:32:36 +0600

    kitchen

--- 10.JS-DOM/03-dom-project/02-eCommerce/index.js
+++ 10.JS-DOM/03-dom-project/02-eCommerce/index.js
@@ -1,11 +1,8 @@
 //reaction counting func
-
-document.getElementById("react").addEventListener('click', (e) => {
+document.getElementById("react").addEventListener("click", (e) => {
     const reactCountId = document.getElementById("react-count");
-    const reactCountValue = reactCountId.innerText;
-    const reactCount = parseFloat(reactCountValue);
-
-    const totalReact = reactCount + 1;
-
-    reactCountId.innerText = totalReact;
+    console.log(e.target.value);
+ 
 });
+
+
