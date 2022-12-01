86862c58f808d3e1728e79bdcd781858c4ee7230
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-01 18:30:28 +0600

    kitchen

--- 10.JS-DOM/03-dom-project/02-eCommerce/index.js
+++ 10.JS-DOM/03-dom-project/02-eCommerce/index.js
@@ -1,8 +1,48 @@
-//reaction counting func
-document.getElementById("react").addEventListener("click", (e) => {
-    const reactCountId = document.getElementById("react-count");
-    console.log(e.target.value);
- 
+//reaction counting function
+const reactionCount = (id) => {
+    const getId = document.getElementById(id);
+    const idValue = getId.value;
+    const toInt = parseFloat(idValue);
+    const total = toInt + 1;
+    getId.value = total;
+};
+//get reaction button function
+const reaction = (id, count) => {
+    document.getElementById(id).addEventListener("click", () => {
+        reactionCount(count);
+    });
+};
+//love reaction
+reaction("love1", "loveCount1");
+
+//star reaction
+reaction("star1", "starCount1");
+
+//add items function
+const addItems = (id) => {
+    const getId = document.getElementById(id);
+    const getValue = getId.value;
+    const toInt = parseFloat(getValue);
+    const total = toInt + 1;
+    if (toInt >= 0) {
+        getId.value = total;
+    }
+};
+//sub items function
+const subItems = (id) => {
+    const getId = document.getElementById(id);
+    const getValue = getId.value;
+    const toInt = parseFloat(getValue);
+    const sub = toInt - 1;
+    if (toInt > 0) {
+        getId.value = sub;
+    }
+    
+};
+
+document.getElementById("addItems1").addEventListener("click", () => {
+    addItems("countItems1");
 });
-
-
+document.getElementById("subItems1").addEventListener("click", () => {
+    subItems("countItems1");
+});
