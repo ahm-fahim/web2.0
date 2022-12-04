e1faebce6ad2b56c21b8a138e622b06762285ec9
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-04 18:38:32 +0600

    js api

--- 11-API/displayData.js
+++ 11-API/displayData.js
@@ -1,3 +1,19 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+function getUser() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => displayUser(data));
+}
+
+function displayUser(data) {
+    const ul = document.getElementById("users-list");
+    for (const user of data) {
+        console.log(user);
+        const li = document.createElement("li");
+        li.innerText = `name: ${user.name} \n email: ${user.email} \n address:\ncity:${user.address.city}\nstreet: ${user.address.street} \n\n `;
+        ul.appendChild(li);
+    }
+}
