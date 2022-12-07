1d37c15176e185498d4e1a3387d7e6eedb81624e
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-08 02:02:37 +0600

    hungry

--- 10.JS-DOM/03-dom-project/04.hungry/index.js
+++ 10.JS-DOM/03-dom-project/04.hungry/index.js
@@ -2,14 +2,28 @@ const searchMeal = () => {
     const getMealName = document.getElementById("search-meal");
     const mealName = getMealName.value;
 
-    console.log(mealName);
     const getMeals = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
 
     fetch(getMeals)
         .then((res) => res.json())
-        .then((data) => searchMealResult(data));
+        .then((data) => searchMealResult(data.meals));
 };
 
-const searchMealResult = (meal) => {
-    console.log(meal);
+const searchMealResult = (meals) => {
+    const displayMeals = document.getElementById("display-search-meal");
+    meals.forEach((meal) => {
+        const div = document.createElement("div");
+        div.classList.add("col");
+
+        console.log(meal);
+        div.innerHTML = `
+            <div class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                </div>
+            </div>
+        `;
+        displayMeals.appendChild(div);
+    });
 };
