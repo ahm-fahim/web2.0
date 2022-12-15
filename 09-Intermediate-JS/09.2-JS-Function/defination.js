3c9f406779cfdfebe956f005d555edb082f1d8de
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-15 21:54:11 +0600

    function defination

--- 09-Intermediate-JS/09.2-JS-Function/defination.js
+++ 09-Intermediate-JS/09.2-JS-Function/defination.js
@@ -1,3 +1,34 @@
 function functionName(parameters) {
     //code to be executed
 }
+
+//function expressions
+
+const x = function (a, b) {
+    return a * b;
+};
+
+//function constructor
+const myFunc = new Function("a", "b", "return a * b");
+
+let constructor = myFunc(4, 3);
+console.log(constructor);
+
+// self invoking 
+(function () {
+    let invoke = "Hello !";
+    console.log(invoke);
+})();
+
+//function are objects
+
+function funcObj(a, b) {
+    return arguments.length;
+}
+
+console.log(funcObj(3, 5));
+
+//arrow functions
+
+// ES6
+const arrow = (a, b) => a + b;
