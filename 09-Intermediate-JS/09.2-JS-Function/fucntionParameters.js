6d4c7839bb14744969a4b0ce43572605dbe5816b
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-17 20:39:08 +0600

    func parameter

--- 09-Intermediate-JS/09.2-JS-Function/fucntionParameters.js
+++ 09-Intermediate-JS/09.2-JS-Function/fucntionParameters.js
@@ -4,8 +4,22 @@ function myFunc(x, y) {
     }
 }
 
-let count = 0;
-setInterval(() => {
-    count++;
-    console.log(count);
-}, 1000);
+// let count = 0;
+// setInterval(() => {
+//     count++;
+//     console.log(count);
+// }, 1000);
+
+
+// argument Object
+
+function findMax() {
+    let max = -Infinity;
+    for (let i = 0; i < arguments.length; i++){
+        if (arguments[i] > max) {
+            max = arguments[i];
+        }
+    }
+    return max;
+}
+console.log(findMax(1, 222, 44, 333, 55, 553, 643, 43));
