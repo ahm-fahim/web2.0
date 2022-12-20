89b9c9fe2305fb0f483fec9e0a7075901134a156
mdFahimMorshed <contactahmfahim@gmail.com>
2022-12-21 01:40:08 +0600

    web 2.0 updated

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function myFunction() {
 function loadGoogleTranslate() {
     new google.translate.TranslateElement("google_translate");
 }
+
+//particles
+window.onload = function () {
+    Particles.init({
+        selector: ".background",
+        color: "#c8cdcf",
+        sizeVariations: 3,
+        connectParticles: true,
+        maxParticles: 50,
+        minParticles: 10,
+    });
+};
